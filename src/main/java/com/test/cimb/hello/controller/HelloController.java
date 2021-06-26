faa614419a703a326cf15e338abb8ed1ff26f166
jeffrey wang <jeffrey@gitlab.dev>
2021-06-26 13:47:05 +0800

    first commit

---
+++ src/main/java/com/test/cimb/hello/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.test.cimb.hello.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public ResponseEntity<?> hello(){
+        //for testing test test
+        //for test gitlab gitlogin
+
+        return ResponseEntity.ok().body("Hello World");
+    }
+
+
+
+}
